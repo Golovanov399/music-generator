8d471e8b5b1d9a2b2ef985fe91b901f5e5eeedb9
Medanya <medanya@inbox.ru>
2014-10-01 02:31:10 +0400

    Create header.h

---
+++ header.h
@@ -0,0 +1,28 @@
+const double REAL_FREQUENCY = 1.0;
+
+enum Mode {major, minor};
+
+class Note {
+	int frequency_;			
+	double duration_;		
+	double volume_;
+	
+public :
+	Note();
+	Note(int frequency, double duration, double volume) {frequency_ = frequency;	duration_ = duration; volume_ = volume;}
+	double getRealFrequency() {return frequency_*REAL_FREQUENCY;}
+	double getDuration() {return duration_ ;}
+	double getVolume() {return volume_;}
+} ;
+
+class Scale {
+	Note note_;
+	Mode mode_;
+	
+public :
+	Scale(Note note, Mode mode) {
+		note_ = note;
+		mode_ = mode;
+	}
+	Mode getMode() {return mode_;}
+} ;
