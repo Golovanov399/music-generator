2118b7050410d3ede13e9e261def0cd4b32518aa
MTurbin <max.turbin95@gmail.com>
2014-10-03 15:43:32 +0400

    Create Track.h

---
+++ Track.h
@@ -0,0 +1,22 @@
+#ifndef __Track__
+
+#define __Track__
+
+#include <vector>
+
+class Track
+{
+	std::vector<double> wave_;
+
+public:
+	Track();
+	Track(const std::vector<double>& wave);
+//	Track(const std::vector<std::pair<Note, double> >& sequence); - ToDo
+	int getLength() const;
+	double getValue(int i) const;
+
+	Track add(int offset, const Track& delta) const;
+	void drop() const;
+};
+
+#endif
