2ff9489e3625553967f8ffdc09373ce32c67ef40
MTurbin <max.turbin95@gmail.com>
2014-10-03 16:30:59 +0400

    code style
    code style fixed

--- Track.cpp
+++ Track.cpp
@@ -22,10 +22,10 @@ int Track::getLength() const
 	return wave_.size();
 }
 
-double Track::getValue(int i) const
+double Track::getValue(int index) const
 {
-	assert(0 <= i && i < (int)wave_.size());
-	return wave_[i];
+	assert(0 <= index && index < (int)wave_.size());
+	return wave_[index];
 }
 
 Track Track::add(int offset, const Track& delta) const
@@ -44,40 +44,40 @@ Track Track::add(int offset, const Track& delta) const
 
 void Track::drop() const
 {
-	FILE* pFile = fopen("sample.wav", "wb");
-	int	ChunkID = 0x46464952, // RIFF
-		ChunkSize,
-		Format = 0x45564157, // WAVE
-		Subchunk1ID = 0x20746d66, // "fmt "
-		Subchunk1Size = 16; // for PCM
-	short	AudioFormat = 1,
-		NumChannels = 1,
-		BitsPerSample = 16;
-	int	SampleRate = SAMPLE_RATE,
-		ByteRate = SampleRate * NumChannels * BitsPerSample / 8;
-	short	BlockAlign = NumChannels * BitsPerSample / 8;
-	int	Subchunk2ID = 0x61746164, // data
-		Subchunk2Size = wave_.size() * NumChannels * BitsPerSample / 8;
-		ChunkSize = 4 + (8 + Subchunk1Size) + (8 + Subchunk2Size);
+	FILE* p_file = fopen("sample.wav", "wb");
+	int	chunk_id = 0x46464952, // RIFF
+		chunk_size,
+		format = 0x45564157, // WAVE
+		subchunk1_id = 0x20746d66, // "fmt "
+		subchunk1_size = 16; // for PCM
+	short	audio_format = 1,
+		num_channels = 1,
+		bits_per_sample = 16;
+	int	sample_rate = SAMPLE_RATE,
+		byte_rate = sample_rate * num_channels * bits_per_sample / 8;
+	short	block_align = num_channels * bits_per_sample / 8;
+	int	subchunk2_id = 0x61746164, // data
+		subchunk2_size = wave_.size() * num_channels * bits_per_sample / 8;
+		chunk_size = 4 + (8 + subchunk1_size) + (8 + subchunk2_size);
 
-	fwrite(&ChunkID, sizeof(int), 1, pFile);	
-	fwrite(&ChunkSize, sizeof(int), 1, pFile);
-	fwrite(&Format, sizeof(int), 1, pFile);
-	fwrite(&Subchunk1ID, sizeof(int), 1, pFile);
-	fwrite(&Subchunk1Size, sizeof(int), 1, pFile);
-	fwrite(&AudioFormat, sizeof(short), 1, pFile);
-	fwrite(&NumChannels, sizeof(short), 1, pFile);
-	fwrite(&SampleRate, sizeof(int), 1, pFile);
-	fwrite(&ByteRate, sizeof(int), 1, pFile);
-	fwrite(&BlockAlign, sizeof(short), 1, pFile);
-	fwrite(&BitsPerSample, sizeof(short), 1, pFile);
-	fwrite(&Subchunk2ID, sizeof(int), 1, pFile);
-	fwrite(&Subchunk2Size, sizeof(int), 1, pFile);
+	fwrite(&chunk_id, sizeof(int), 1, p_file);	
+	fwrite(&chunk_size, sizeof(int), 1, p_file);
+	fwrite(&format, sizeof(int), 1, p_file);
+	fwrite(&subchunk1_id, sizeof(int), 1, p_file);
+	fwrite(&subchunk1_size, sizeof(int), 1, p_file);
+	fwrite(&audio_format, sizeof(short), 1, p_file);
+	fwrite(&num_channels, sizeof(short), 1, p_file);
+	fwrite(&sample_rate, sizeof(int), 1, p_file);
+	fwrite(&byte_rate, sizeof(int), 1, p_File);
+	fwrite(&block_align, sizeof(short), 1, p_file);
+	fwrite(&bits_per_sample, sizeof(short), 1, p_file);
+	fwrite(&subchunk2_id, sizeof(int), 1, p_file);
+	fwrite(&subchunk2_size, sizeof(int), 1, p_file);
 
 
-	short Data[wave_.size()];
+	short data[wave_.size()];
 	for (int i = 0; i < (int)wave_.size(); i++)
-		Data[i] = (short)(wave_[i] * 32767 / MAX_AMPLITUDE);
-	fwrite(Data, sizeof(short), wave_.size(), pFile);
-	fclose(pFile);
+		data[i] = (short)(wave_[i] * 32767 / MAX_AMPLITUDE);
+	fwrite(data, sizeof(short), wave_.size(), pFile);
+	fclose(p_file);
 }
--- Track.h
+++ Track.h
@@ -13,7 +13,7 @@ public:
 	Track(const std::vector<double>& wave);
 //	Track(const std::vector<std::pair<Note, double> >& sequence); - ToDo
 	int getLength() const;
-	double getValue(int i) const;
+	double getValue(int index) const;
 
 	Track add(int offset, const Track& delta) const;
 	void drop() const;
