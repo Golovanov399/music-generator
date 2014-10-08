cbf48ac97af1674467128f164cea4c5dc3f5150f
Poreykin <zhenyaporeykin@yandex.ru>
2014-10-09 03:02:12 +0600

    Update Note.h

--- Note.h
+++ Note.h
@@ -21,7 +21,7 @@ public :
 	Note() {frequency_ = 440; duration_ = 1; volume_ = MAX_AMPLITUDE;};
 	Note(int frequency, double duration = 1, double volume = MAX_AMPLITUDE) {frequency_ = frequency;	duration_ = duration; volume_ = volume;}
 	double getRealFrequency() const {return pow(2, 1.0 * (frequency_ - 9) / 12) * REAL_FREQUENCY;}
-	double getFrequency() const {return frequency_;}
+	int getFrequency() const {return frequency_;}
 	double getDuration() const {return duration_;}
 	double getVolume() const {return volume_;}
 } ;
