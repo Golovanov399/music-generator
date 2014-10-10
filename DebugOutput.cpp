7d15b4aeb67530a4b5231670ddb06670a1c6e89e
MTurbin <max.turbin95@gmail.com>
2014-10-10 15:51:27 +0400

    .h and .cpp separation
    Also - can read note sequence from file.

---
+++ DebugOutput.cpp
@@ -0,0 +1,62 @@
+#include <cstdio>
+#include <vector>
+#include "Note.h"
+#include "Track.h"
+
+void printNote(FILE* OutputFile, const Note& note)
+{
+	fprintf(OutputFile, "%d %f %f", note.getFrequency(), note.getDuration(), note.getVolume());
+	return;
+}
+
+void printMode(FILE* OutputFile, const Mode& mode)
+{
+	if (mode == major)
+		fprintf(OutputFile, "major");
+	if (mode == minor)
+		fprintf(OutputFile, "minor");
+	return;
+}
+
+void printChord(FILE* OutputFile, const Chord& chord)
+{
+	printNote(OutputFile, chord.getNote());
+	fprintf(OutputFile, " ");
+	printMode(OutputFile, chord.getMode());
+	return;
+}
+
+void printNoteSequence(FILE* OutputFile, const std::vector<std::pair<Note, double> >& NoteSequence)
+{
+	int SequenceLength = NoteSequence.size();
+	for (int i = 0; i < SequenceLength; ++i) {
+		printNote(OutputFile, NoteSequence[i].first);
+		fprintf(OutputFile, " %f\n", NoteSequence[i].second);
+	}
+	return;
+}
+
+void printChordSequence(FILE* OutputFile, const std::vector<std::pair<Chord, double> >& ChordSequence)
+{
+	int SequenceLength = ChordSequence.size();
+	for (int i = 0; i < SequenceLength; ++i) {
+		printChord(OutputFile, ChordSequence[i].first);
+		fprintf(OutputFile, " %f\n", ChordSequence[i].second);
+	}
+	return;
+}
+
+void printTrack(FILE* OutputFile, const Track& track)
+{
+	int TrackLength = track.getLength();
+	for (int i = 0; i < TrackLength; ++i) {
+		fprintf(OutputFile, "%f\n", track.getValue(i));
+	}
+}
+
+void scanNoteSequence(FILE* InputFile, std::vector<std::pair<Note, double> >& NoteSequence)
+{
+	int NoteFrequency; double NoteDuration, Amplitude, Position;
+	while (fscanf(InputFile, "%d%lf%lf%lf", &NoteFrequency, &NoteDuration, &Amplitude, &Position))
+	        NoteSequence.push_back(make_pair(Note(NoteFrequency, NoteDuration, Amplitude), Position));
+}
--- DebugOutput.h
+++ DebugOutput.h
@@ -7,55 +7,12 @@
 #include "Note.h"
 #include "Track.h"
 
-void printNote(FILE* OutputFile, const Note& note)
-{
-	fprintf(OutputFile, "%d %f %f", note.getFrequency(), note.getDuration(), note.getVolume());
-	return;
-}
-
-void printMode(FILE* OutputFile, const Mode& mode)
-{
-	if (mode == major)
-		fprintf(OutputFile, "major");
-	if (mode == minor)
-		fprintf(OutputFile, "minor");
-	return;
-}
-
-void printChord(FILE* OutputFile, const Chord& chord)
-{
-	printNote(OutputFile, chord.getNote());
-	fprintf(OutputFile, " ");
-	printMode(OutputFile, chord.getMode());
-	return;
-}
-
-void printNoteSequence(FILE* OutputFile, const std::vector<std::pair<Note, double> >& NoteSequence)
-{
-	int SequenceLength = NoteSequence.size();
-	for (int i = 0; i < SequenceLength; ++i) {
-		printNote(OutputFile, NoteSequence[i].first);
-		fprintf(OutputFile, " %f\n", NoteSequence[i].second);
-	}
-	return;
-}
-
-void printChordSequence(FILE* OutputFile, const std::vector<std::pair<Chord, double> >& ChordSequence)
-{
-	int SequenceLength = ChordSequence.size();
-	for (int i = 0; i < SequenceLength; ++i) {
-		printChord(OutputFile, ChordSequence[i].first);
-		fprintf(OutputFile, " %f\n", ChordSequence[i].second);
-	}
-	return;
-}
-
-void printTrack(FILE* OutputFile, const Track& track)
-{
-	int TrackLength = track.getLength();
-	for (int i = 0; i < TrackLength; ++i) {
-		fprintf(OutputFile, "%f\n", track.getValue(i));
-	}
-}
+void printNote(FILE* OutputFile, const Note& note);
+void printMode(FILE* OutputFile, const Mode& mode);
+void printChord(FILE* OutputFile, const Chord& chord);
+void printNoteSequence(FILE* OutputFile, const std::vector<std::pair<Note, double> >& NoteSequence);
+void printChordSequence(FILE* OutputFile, const std::vector<std::pair<Chord, double> >& ChordSequence);
+void printTrack(FILE* OutputFile, const Track& track);
+void scanNoteSequence(FILE* InputFile, std::vector<std::pair<Note, double> >& NoteSequence);
 
 #endif
