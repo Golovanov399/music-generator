116f0047f44527f507027b507c1c2c2e19db68ed
Poreykin <zhenyaporeykin@yandex.ru>
2014-10-23 02:52:56 +0600

    Create Instrument.cpp

---
+++ Instrument.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include "Instrument.h"
+
+Instrument::Instrument(const std::vector<double>& harmonics, double attackTime, double decayTime, double sustainLevel, double releaseTime)
+{
+	harmonics_ = harmonics;
+	attackTime_ = attackTime;
+	decayTime_ = decayTime;
+	sustainLevel_ = sustainLevel;
+	releaseTime_ = releaseTime;
+	return;
+}
+
+double Instrument::ADSR(double time, double duration) const // time in seconds
+{
+	if (time <= attackTime_)
+		return (attackTime_ == 0 ? 1 : time / attackTime_);
+	if (time <= attackTime_ + decayTime_)
+		return (1 - (1 - sustainLevel_) * (time - attackTime_) / decayTime_);
+	if (time <= duration)
+		return sustainLevel_;
+	if (time <= duration + releaseTime_)
+		return (sustainLevel_ * (duration + releaseTime_ - time) / releaseTime_);
+	return 0;
+}
+
+double Instrument::getWaveValue(double frequency, double volume, double time, double duration) const // time in seconds/SAMPLE_RATE; duration in seconds
+{
+	double waveValue = sin(frequency * time);
+	for (int i = 0; i < harmonics_.size(); ++i)
+		waveValue += (sin(harmonics_[i] * time) / (double) i);
+	double timeInSeconds = time / (double) SAMPLE_RATE;
+	return (ADSR(timeInSeconds, duration) * volume * waveValue);
+}
