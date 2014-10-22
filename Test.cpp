b413bba99a4beeee781e4b91358b8b1d33c9cac2
Poreykin <zhenyaporeykin@yandex.ru>
2014-10-23 01:36:39 +0600

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -7,7 +7,7 @@
 void testADSR(int length)
 {
 	for (int i = 0; i <= 2 * length; ++i)
-		fprintf(stderr, "%f\n", ADSR(i, (double) i / (double) length));
+		fprintf(stderr, "%f\n", ADSR(i, (double) i / (double) length, 1.0));
 	return;
 }
 
