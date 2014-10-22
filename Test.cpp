54d838f4143d078ea95c7e4d3b8401ed130c5923
Poreykin <zhenyaporeykin@yandex.ru>
2014-10-23 01:08:06 +0600

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include "Test.h"
+#include "Track.h"
+
+void testADSR(int length)
+{
+	for (int i = 0; i <= 2 * length; ++i)
+		fprintf(stderr, "%f\n", ADSR(i, (double) i / (double) length));
+	return;
+}
+
+int main()
+{
+	testADSR(10);
+	return 0;	
+}
