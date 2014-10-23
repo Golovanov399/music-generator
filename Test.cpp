65aa515d1cfca22babd49c307cd3bb6b9636ae49
Poreykin <zhenyaporeykin@yandex.ru>
2014-10-24 00:02:39 +0600

    Update Test.cpp
    Do not compile it with main

--- Test.cpp
+++ Test.cpp
@@ -1,17 +1,23 @@
+#include <cmath>
+#include <cstdio>
 #include <cstdlib>
 #include <vector>
-#include "Test.h"
+#include "Instrument.h"
 #include "Track.h"
+#include "Test.h"
 
-void testADSR(int length)
+void testADSR()
 {
-	for (int i = 0; i <= 2 * length; ++i)
-		fprintf(stderr, "%f\n", ADSR(i, (double) i / (double) length, 1.0));
+	Instrument unknown(std::vector<double>(0), 0.5, 0.5, 0.5, 0.5);
+	double answers[] = {0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+	for (int i = 0; i <= 20; ++i)
+		if (fabs(unknown.ADSR((double) i / 10.0, 1.0) - answers[i]) >= 0.000001)
+			fprintf(stderr, "ADSR(%d) = %f (right answer is %f)\n", i, unknown.ADSR((double) i / 10.0, 1.0), answers[i]);
 	return;
 }
 
 int main()
 {
-	testADSR(10);
+	testADSR();
 	return 0;	
 }
