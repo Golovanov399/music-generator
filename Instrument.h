a98090746f3f4f45d71cf2e6af24ae07995ed417
Poreykin <zhenyaporeykin@yandex.ru>
2014-11-07 16:51:17 +0500

    Update Instrument.h

--- Instrument.h
+++ Instrument.h
@@ -18,8 +18,19 @@ public:
 	Instrument(const std::vector<double>& harmonics, double attackTime, double decayTime, double sustainLevel, double releaseTime);
 
 	double getReleaseTime() const;
+	double getAttackWaveValue(double time) const;
+	double getDecayWaveValue(double time) const;
+	double getReleaseWaveValue(double time) const;
 	double ADSR(double time, double duration) const;
 	double getWaveValue(double frequency, double volume, double time, double duration) const;
 };
+
+const Instrument noInstrument(std::vector<double>(0), 0.0, 0.0, 1.0, 0.0);
+
+const double sawHarmonics[8] = {1.0/2.0, 1.0/3.0, 1.0/4.0, 1.0/5.0, 1.0/6.0, 1.0/7.0, 1.0/8.0, 1.0/9.0};
+const Instrument sawWave(std::vector<double>(sawHarmonics, sawHarmonics + 8), 0.0, 0.0, 1.0, 0.0);
+
+const double squareHarmonics[8] = {0, 1.0/3.0, 0, 1.0/5.0, 0, 1.0/7.0, 0, 1.0/9.0};
+const Instrument squareWave(std::vector<double>(squareHarmonics, squareHarmonics + 8), 0.0, 0.0, 1.0, 0.0);
 
 #endif
