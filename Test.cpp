66b27ffac5a94c4cee46cccd82e3db7c07da6a62
Poreykin <zhenyaporeykin@yandex.ru>
2014-11-25 23:07:20 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -8,21 +8,20 @@
 
 void testADSR()
 {
-	Instrument unknown(std::vector<double>(0), 0.5, 0.5, 0.5, 0.5);
+	Instrument unknown(0.5, 0.5, 0.5, 0.5);
 	double answers[] = {0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 	for (int i = 0; i <= 20; ++i)
-		if (fabs(unknown.ADSR((double) i / 10.0, 1.0) - answers[i]) >= 0.000001)
-			fprintf(stderr, "ADSR(%d) = %f (right answer is %f)\n", i, unknown.ADSR((double) i / 10.0, 1.0), answers[i]);
+		if (fabs(unknown.ADSR(440, (double) i / 10.0, 1.0) - answers[i]) >= 0.000001)
+			fprintf(stderr, "ADSR(%d) = %f (right answer is %f)\n", i, unknown.ADSR(440, (double) i / 10.0, 1.0), answers[i]);
 	return;
 }
 
 Instrument testInstrument()
 {
 	double A, D, S, R;
-	std::vector<double> harm(0);
+	std::map<double, double> harm;
 	int size;
 	scanf("%d", &size);
-	harm.resize(size);
 	for (int i = 0; i < size; i++)
 		scanf("%lf", &harm[i]);
 	scanf("%lf%lf%lf%lf", &A, &D, &S, &R);
