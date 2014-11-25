a45438d42f0f58144ca3d83364cb3c917754ba71
Poreykin <zhenyaporeykin@yandex.ru>
2014-11-25 23:07:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,29 +9,24 @@
 
 int main()
 {
-	Generator1* gen = new Generator1();
-	std::vector<std::pair<Note, double> > Melody = gen->generateMelody();
-	delete gen;
-/*	FILE* OutputFile1 = fopen("NoteSequence.txt", "w");
+	Generator1 gen;
+	std::vector<std::pair<Note, double> > Melody = gen.generateMelody();
+/*
+	std::vector<std::pair<Note, double> > Melody;
+	Melody.push_back(std::pair<Note, double>(Note(9, 1.0, 25), 0));
+	//Melody.push_back(std::pair<Note, double>(Note(10, 1.0, 25), 1));
+*/
+	FILE* OutputFile1 = fopen("NoteSequence.txt", "w");
 	printNoteSequence(OutputFile1, Melody);
 	fclose(OutputFile1);
 
-	Instrument noInstrument(std::vector<double>(0), 0.0, 0.0, 1.0, 0.0);
-	vector<double> harmonics(10);
-	for (int i = 0; i < 10; i++)
-		harmonics[i] = (i & 2 ? (1.0 / (double)(i + 2)) : 0);
-	Instrument sawInstrument(harmonics, 0.0, 0.0, 1.0, 0.0);
-
-	// Track MelodyTrack(Melody, noInstrument);
-	Track MelodyTrack(Melody, sawInstrument);
+	Track MelodyTrack(Melody, Instrument());
 	MelodyTrack.normalize();
 
 	FILE* OutputFile2 = fopen("Track.txt", "w");
 	printTrack(OutputFile2, MelodyTrack);
 	fclose(OutputFile2);
-*/
 
-	Track MelodyTrack(Melody, testInstrument());	
 	MelodyTrack.drop();
 
 	return 0;
