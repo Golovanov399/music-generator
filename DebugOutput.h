c26aceeb70c5dee050030aca7de4837eb3a06649
Poreykin <zhenyaporeykin@yandex.ru>
2014-11-27 15:49:49 +0300

    Update DebugOutput.h

--- DebugOutput.h
+++ DebugOutput.h
@@ -1,18 +1,16 @@
-#ifndef __DebugOutput__
-
-#define __DebugOutput__
+#pragma once
 
 #include <cstdio>
+#include <fstream>
 #include <vector>
 #include "Note.h"
 #include "Track.h"
 
-void printNote(FILE* OutputFile, const Note& note);
-void printMode(FILE* OutputFile, const Mode& mode);
-void printChord(FILE* OutputFile, const Chord& chord);
-void printNoteSequence(FILE* OutputFile, const std::vector<std::pair<Note, double> >& NoteSequence);
-void printChordSequence(FILE* OutputFile, const std::vector<std::pair<Chord, double> >& ChordSequence);
-void printTrack(FILE* OutputFile, const Track& track);
-void scanNoteSequence(FILE* InputFile, std::vector<std::pair<Note, double> >& NoteSequence);
-
-#endif
+void printNote(std::ofstream* outputFile, const Note& note);
+void printMode(std::ofstream* outputFile, const Mode& mode);
+void printChord(std::ofstream* outputFile, const Chord& chord);
+void printNoteSequence(std::ofstream* outputFile, const std::vector<std::pair<Note, double> >& NoteSequence);
+void printChordSequence(std::ofstream* outputFile, const std::vector<std::pair<Chord, double> >& ChordSequence);
+void printInstrument(std::ofstream* outputFile, const Instrument& instrument);
+void printTrack(std::ofstream* outputFile, const Track& track);
+void scanNoteSequence(FILE* inputFile, std::vector<std::pair<Note, double> >& NoteSequence);
