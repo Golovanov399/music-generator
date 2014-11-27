b6241ff6363f13915e5cb54ca0a050a067c34a46
Poreykin <zhenyaporeykin@yandex.ru>
2014-11-27 15:49:59 +0300

    Update DebugOutput.cpp

--- DebugOutput.cpp
+++ DebugOutput.cpp
@@ -1,67 +1,76 @@
+#include <cassert>
+#include <cmath>
 #include <cstdio>
+#include <fstream>
 #include <vector>
 #include "DebugOutput.h"
 #include "Note.h"
 #include "Track.h"
-#include <cassert>
-#include <cmath>
 
-void printNote(FILE* OutputFile, const Note& note)
+void printNote(std::ofstream* outputFile, const Note& note)
 {
-	fprintf(OutputFile, "%d %lf %lf", note.getFrequency(), note.getDuration(), note.getVolume());
-	return;
+	(*outputFile)<<note.getFrequency()<<" "<<note.getDuration()<<" "<<note.getVolume();
 }
 
-void printMode(FILE* OutputFile, const Mode& mode)
+void printMode(std::ofstream* outputFile, const Mode& mode)
 {
 	if (mode == MAJOR)
-		fprintf(OutputFile, "MAJOR");
+		(*outputFile)<<"MAJOR";
 	if (mode == MINOR)
-		fprintf(OutputFile, "MINOR");
-	return;
+		(*outputFile)<<"MINOR";
 }
 
-void printChord(FILE* OutputFile, const Chord& chord)
+void printChord(std::ofstream* outputFile, const Chord& chord)
 {
-	printNote(OutputFile, chord.getNote());
-	fprintf(OutputFile, " ");
-	printMode(OutputFile, chord.getMode());
-	return;
+	printNote(outputFile, chord.getNote());
+	(*outputFile)<<" ";
+	printMode(outputFile, chord.getMode());
 }
 
-void printNoteSequence(FILE* OutputFile, const std::vector<std::pair<Note, double> >& NoteSequence)
+void printNoteSequence(std::ofstream* outputFile, const std::vector<std::pair<Note, double> >& NoteSequence)
 {
 	int SequenceLength = NoteSequence.size();
 	for (int i = 0; i < SequenceLength; ++i) {
-		printNote(OutputFile, NoteSequence[i].first);
-		fprintf(OutputFile, " %f\n", NoteSequence[i].second);
-	}
-	return;
-}
-
-void printChordSequence(FILE* OutputFile, const std::vector<std::pair<Chord, double> >& ChordSequence)
-{
-	int SequenceLength = ChordSequence.size();
-	for (int i = 0; i < SequenceLength; ++i) {
-		printChord(OutputFile, ChordSequence[i].first);
-		fprintf(OutputFile, " %f\n", ChordSequence[i].second);
-	}
-	return;
-}
-
-void printTrack(FILE* OutputFile, const Track& track)
-{
-	int TrackLength = track.getLength();
-	for (int i = 0; i < TrackLength; ++i) {
-//		fprintf(stderr, "%.6lf\n", track.getValue(i));
-		assert(fabs(track.getValue(i)) < MAX_AMPLITUDE);
-		fprintf(OutputFile, "%lf\n", track.getValue(i));
+		printNote(outputFile, NoteSequence[i].first);
+		(*outputFile)<<" "<<NoteSequence[i].second<<"\n";
 	}
 }
 
-void scanNoteSequence(FILE* InputFile, std::vector<std::pair<Note, double> >& NoteSequence)
+void printChordSequence(std::ofstream* outputFile, const std::vector<std::pair<Chord, double> >& ChordSequence)
 {
-	int NoteFrequency; double NoteDuration, Amplitude, Position;
-	while (fscanf(InputFile, "%d%lf%lf%lf", &NoteFrequency, &NoteDuration, &Amplitude, &Position) == 4)
+	int sequenceLength = ChordSequence.size();
+	for (int i = 0; i < sequenceLength; ++i) {
+		printChord(outputFile, ChordSequence[i].first);
+		(*outputFile)<<" "<<ChordSequence[i].second<<"\n";
+	}
+}
+
+void printInstrument(std::ofstream* outputFile, const Instrument& instrument)
+{
+	std::map<double, double> harmonics = instrument.getHarmonics();
+	(*outputFile)<<harmonics.size()<<"\n";
+	for (auto harmonic : harmonics)
+		(*outputFile)<<harmonic.first<<" "<<harmonic.second<<"\n";
+	(*outputFile)<<instrument.getModulationIndex0()<<" "<<instrument.getModulationRatio()<<"\n";
+	(*outputFile)<<instrument.getAttackTime()<<" "<<
+			instrument.getDecayTime()<<" "<<
+			instrument.getSustainLevel()<<" "<<
+			instrument.getReleaseTime()<<"\n";
+}
+
+void printTrack(std::ofstream* outputFile, const Track& track)
+{
+	int trackLength = track.getLength();
+	for (int i = 0; i < trackLength; ++i) {
+		assert(fabs(track.getValue(i)) <= MAX_AMPLITUDE);
+		(*outputFile)<<track.getValue(i)<<"\n";
+	}
+}
+
+void scanNoteSequence(FILE* inputFile, std::vector<std::pair<Note, double> >& NoteSequence)
+{
+	int NoteFrequency;
+	double NoteDuration, Amplitude, Position;
+	while (fscanf(inputFile, "%d%lf%lf%lf", &NoteFrequency, &NoteDuration, &Amplitude, &Position) == 4)
 	        NoteSequence.push_back(std::make_pair(Note(NoteFrequency, NoteDuration, Amplitude), Position));
 }
