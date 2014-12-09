cdeaf96645f2fcf734f24993d8763da5ab37919c
Poreykin <zhenyaporeykin@yandex.ru>
2014-12-10 02:18:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,20 +13,23 @@
 int main()
 {
 	Generator2 gen;
-	std::vector<std::pair<Note, double> > Melody = gen.generateMelody();
+	Melody melody = gen.generateMelody();
 
-	std::ofstream OutputFile1("NoteSequence.txt", std::ofstream::out);
-	printNoteSequence(&OutputFile1, Melody);
+	std::ofstream OutputFile1("Accompaniment.txt", std::ofstream::out);
+	printNoteSequence(&OutputFile1, melody.getAccompaniment());
+	std::ofstream OutputFile2("MainTheme.txt", std::ofstream::out);
+	printNoteSequence(&OutputFile2, melody.getMainTheme());
 
-	Track melodyTrack(Melody, *randomInstrument());
+	//Track melodyTrack(melody, *randomInstrument(), *randomInstrument());
+	Track melodyTrack(melody, Piano(), Piano());
 	Tremolo effect(0.0, 0.0, 0.2, 3);
 	Echo effect2(0.0, 0.0, 0.6, 0.4);
 	//effect.applyEffect(melodyTrack);
 	//effect2.applyEffect(melodyTrack);
 	melodyTrack.normalize();
 
-	std::ofstream OutputFile2("Track.txt", std::ofstream::out);
-	printTrack(&OutputFile2, melodyTrack);
+	std::ofstream OutputFile3("Track.txt", std::ofstream::out);
+	printTrack(&OutputFile3, melodyTrack);
 
 	melodyTrack.drop("sample.wav");
 
