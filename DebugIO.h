cd6b9aac5ef7fcad374c8817052321c4e640d1a6
Poreykin <zhenyaporeykin@yandex.ru>
2014-12-10 02:51:32 +0300

    Update DebugIO.h

--- DebugIO.h
+++ DebugIO.h
@@ -6,13 +6,13 @@
 #include "Note.h"
 #include "Track.h"
 
-void printNote(std::ofstream* outputFile, const Note& note);
-void printMode(std::ofstream* outputFile, const Mode& mode);
-void printChord(std::ofstream* outputFile, const Chord& chord);
-void printNoteSequence(std::ofstream* outputFile, const std::vector<std::pair<Note, double> >& NoteSequence);
-void printChordSequence(std::ofstream* outputFile, const std::vector<std::pair<Chord, double> >& ChordSequence);
-void printInstrument(std::ofstream* outputFile, const Instrument& instrument);
-void printTrack(std::ofstream* outputFile, const Track& track);
+void printNote(std::ostream* outputFile, const Note& note);
+void printMode(std::ostream* outputFile, const Mode& mode);
+void printChord(std::ostream* outputFile, const Chord& chord);
+void printNoteSequence(std::ostream* outputFile, const std::vector<std::pair<Note, double> >& NoteSequence);
+void printChordSequence(std::ostream* outputFile, const std::vector<std::pair<Chord, double> >& ChordSequence);
+void printInstrument(std::ostream* outputFile, const Instrument& instrument);
+void printTrack(std::ostream* outputFile, const Track& track);
 
 std::vector<std::pair<Note, double> > scanNoteSequence(std::istream* inputFile);
 Instrument scanInstrument(std::istream* inputFile);
