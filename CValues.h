7696332480cd07cecaf9b6663037c3db1f09ef01
Poreykin <zhenyaporeykin@yandex.ru>
2014-12-10 00:40:20 +0300

    Update CValues.h

--- CValues.h
+++ CValues.h
@@ -2,10 +2,16 @@
 
 #define __CValues__
 
+// math constants
+const double PI = 3.14159265358979;
+const double TWO_PI = 6.2831853071796;
+
+// track constants
 const int SAMPLE_RATE = 44100;
 const double SECONDS_IN_BAR = 2.0;
-const double TWO_PI = 6.2831853071796;
-const double PI = 3.14159265358979;
+
+// instrument comstants
+const int NUMBER_OF_INSTRUMENTS = 4;
 
 //maximum and minimum constants of effects
 const double ECHO_MAX_DELAY = 1.0;
@@ -13,7 +19,5 @@ const double ECHO_MIN_DELAY = 0.1;
 
 const double TREMOLO_MAX_FREQUENCY = 0.5;
 const double TREMOLO_MAX_POWER = 0.3;
-
-
 
 #endif
