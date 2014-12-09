65c26492d4c4f57f25efb7f95ff9070ec0c7d0b8
Poreykin <zhenyaporeykin@yandex.ru>
2014-12-10 00:41:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
 #include "Generator.h"
@@ -17,11 +18,11 @@ int main()
 	std::ofstream OutputFile1("NoteSequence.txt", std::ofstream::out);
 	printNoteSequence(&OutputFile1, Melody);
 
-	Track melodyTrack(Melody, Instrument(Instrument::squareHarmonics(10)));
+	Track melodyTrack(Melody, *randomInstrument());
 	Tremolo effect(0.0, 0.0, 0.2, 3);
 	Echo effect2(0.0, 0.0, 0.6, 0.4);
 	//effect.applyEffect(melodyTrack);
-	effect2.applyEffect(melodyTrack);
+	//effect2.applyEffect(melodyTrack);
 	melodyTrack.normalize();
 
 	std::ofstream OutputFile2("Track.txt", std::ofstream::out);
@@ -29,19 +30,8 @@ int main()
 
 	melodyTrack.drop("sample.wav");
 
-	//testInstrument();
+	//testInstrument(); 
 	//testPiano();
-
-/*	for (int i = 1; i <= 21; ++i)
-	{	
-		std::ifstream infile("Piano" + std::to_string(i) + ".txt", std::ifstream::in);
-		Piano piano = scanPiano(&infile);
-		std::vector<std::pair<Note, double> > Melody(1, std::pair<Note, double>(Note(9, 0.003, 25), 0));
-		Track melodyTrack(Melody, piano);
-		melodyTrack.normalize();
-		std::ofstream OutputFile2("Piano" + std::to_string(i) + " track.txt", std::ofstream::out);
-		printTrack(&OutputFile2, melodyTrack);
-	}
-*/
+	//testWindInstrument();
 	return 0;
 }
