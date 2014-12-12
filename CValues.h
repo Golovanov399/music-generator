4c40c2edfc37a07560d87e4920e157b1fcb824be
MTurbin <max.turbin95@gmail.com>
2014-12-12 14:54:32 +0300

    Executables

--- CValues.h
+++ CValues.h
@@ -7,8 +7,8 @@ const double PI = 3.14159265358979;
 const double TWO_PI = 6.2831853071796;
 
 // generator constants
-const int MIN_SECONDS_IN_BAR = 2.0;
-const int MAX_SECONDS_IN_BAR = 4.0;
+const double MIN_SECONDS_IN_BAR = 2.0;
+const double MAX_SECONDS_IN_BAR = 4.0;
 
 // track constants
 const int SAMPLE_RATE = 44100;
@@ -18,15 +18,31 @@ const double MAX_FREQUENCY = 10000.0;
 const int NUMBER_OF_INSTRUMENTS = 4;
 
 // note constants
-const double REAL_FREQUENCY = 440.0;
+const double REAL_FREQUENCY = 220.0;
 const double MAX_AMPLITUDE = 100.0;
 const double MIN_AMPLITUDE = 0.0;
 
 //maximum and minimum constants of effects
 const double ECHO_MAX_DELAY = 1.0;
 const double ECHO_MIN_DELAY = 0.1;
+const double ECHO_MAX_POWER = 1.0;
+const double ECHO_MIN_POWER = 0.2;
 
-const double TREMOLO_MAX_FREQUENCY = 0.5;
+const double TREMOLO_MAX_FREQUENCY = 2;
+const double TREMOLO_MIN_FREQUENCY = 0.5;
 const double TREMOLO_MAX_POWER = 0.3;
+const double TREMOLO_MIN_POWER = 0.0;
+
+const double FLANGER_MAX_FREQUENCY = 1.0;
+const double FLANGER_MIN_FREQUENCY = 1.0;
+const double FLANGER_MAX_DELAY = 0.012;
+const double FLANGER_MIN_DELAY = 0.006;
+const double FLANGER_MAX_POWER = 0.3;
+const double FLANGER_MIN_POWER = 0.0;
+
+const double DISTORTION_MAX_POWER = 1.0;
+const double DISTORTION_MIN_POWER = 0.0;
+const double DISTORTION_MAX_LEVEL = 1.0;
+const double DISTORTION_MIN_LEVEL = 0.1;
 
 #endif
--- main.cpp
+++ main.cpp
@@ -20,12 +20,21 @@ int main()
 	std::ofstream OutputFile2("MainTheme.txt", std::ofstream::out);
 	printNoteSequence(&OutputFile2, melody.getMainTheme());
 
-	//Track melodyTrack(melody, *randomInstrument(), *randomInstrument());
-	Track melodyTrack(melody, Piano(), Piano());
-	Tremolo effect(0.0, 0.0, 0.2, 3);
+	Track melodyTrack(melody, *randomInstrument(), *randomInstrument());
+	//Track melodyTrack(melody, windInstrument::Organ(2), windInstrument::Organ(2));
+	Tremolo effect(0.0, 0.0, 0.3, 3);
 	Echo effect2(0.0, 0.0, 0.6, 0.4);
+	Flanger effect3(0.0, 0.0, 0.3, 1, 0.01);
+	Distortion effect4(0.0, 0.0, 0.0, 0.2);
 	//effect.applyEffect(melodyTrack);
 	//effect2.applyEffect(melodyTrack);
+	//effect3.applyEffect(melodyTrack);
+	//effect4.applyEffect(melodyTrack);
+	int effect_type = rand() % 5;
+	if (effect_type == 0) effect.applyEffect(melodyTrack);
+	if (effect_type == 1) effect2.applyEffect(melodyTrack);
+	if (effect_type == 2) effect3.applyEffect(melodyTrack);
+	if (effect_type == 3) effect4.applyEffect(melodyTrack);
 	melodyTrack.normalize();
 
 	std::ofstream OutputFile3("Track.txt", std::ofstream::out);
